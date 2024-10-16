b2539ef7cc923bc850e2441b2023568d2c5e47ff
ThoReimann <Thomas.Reimann@tu-dresden.de>
2024-10-16 18:07:23 +0200

    Update Well_capture_zone_ST.py

--- 04_Basic_hydrogeology/Well_capture_zone_ST.py
+++ 04_Basic_hydrogeology/Well_capture_zone_ST.py
@@ -34,7 +34,7 @@ columns = st.columns((1,1), gap = 'large')
 with columns[0]:
     x_scale = st.slider('_Plot scaling in x direction_', 0.1, 10., 0.5, 0.01)
     y_scale = st.slider('_Plot scaling in y direction_', 0.1, 10., 0.5, 0.01)
-    revers = st.toggle('Revers flow direction')
+    revers = st.toggle('Reverse x-axis')
 with columns[1]:
     Q = st.slider('**Pumping rate (m3/s)**', 0., 0.2,0.005, 0.001, format="%5.3f")
     K_slider_value=st.slider('(log of) **Hydr. conductivity (m/s)**', log_min,log_max,-3.0,0.01,format="%4.2f" )
@@ -54,8 +54,8 @@ x_max= 1000 #fixed(x_max),
 ymax = ymax_conf(Q, K, i, b)
 x0   = x0_conf(Q, K, i, b)
 
-x = np.arange(int(x0)-1, x_max,1)
-y = np.arange(-ymax*0.999, ymax, 0.1)
+#x = np.arange(int(x0)-1, x_max,1)
+y = np.arange(-ymax*0.99999, ymax, 0.01)
 
 x_well = 0
 y_well = 0
