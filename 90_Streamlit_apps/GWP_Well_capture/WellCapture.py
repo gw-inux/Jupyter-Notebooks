ab1bb7a503a4ae19f20da1ccd11ae0851d6ab320
ThoReimann <Thomas.Reimann@tu-dresden.de>
2024-10-20 01:22:41 +0200

    updates

--- 90_Streamlit_apps/GWP_Well_capture/WellCapture.py
+++ 90_Streamlit_apps/GWP_Well_capture/WellCapture.py
@@ -28,7 +28,7 @@ st.markdown(
 )
 
 # Open the image from the specified path
-image = Image.open('GWP_Well_capture/assets/images/wellcapturediagram-sm42.png')
+image = Image.open("wellcapturediagram-sm42.png")
 
 left_co, cent_co, last_co = st.columns((20,60,20))
 with cent_co:
