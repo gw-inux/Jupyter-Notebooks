86799ed88ec998dd9212937277ab0df7030f92e6
tyferre <35377566+tyferre@users.noreply.github.com>
2024-10-24 17:28:45 +0000

    Still checking codespace

--- 90_Streamlit_apps/Dewatering_exercise/pages/2_▶️_Step 2️⃣.py
+++ 90_Streamlit_apps/Dewatering_exercise/pages/2_▶️_Step 2️⃣.py
@@ -116,7 +116,7 @@ st.markdown(
     """
     ---
     
-    #### Assignment after step 2. 📃
+    #### Assignment after Step 2. 📃
     * Produce three curves of }_s(t)_ out to two years, one at the distance relevant for each stakeholder, on the same axes.
     * Explain in a clear paragraph why they all have the same general shape, but they are different in the details.
     * Discuss your understanding at this time of the role of a hydrogeologist in negotiating water issues related to dewatering.
