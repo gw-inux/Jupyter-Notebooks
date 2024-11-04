0713f03dced5537578b4e094b03db0c8fc3101b4
ThoReimann <Thomas.Reimann@tu-dresden.de>
2024-11-04 12:42:07 +0100

    Update 03_👉_About.py

--- 90_Streamlit_apps/GWP_Well_capture/pages/03_👉_About.py
+++ 90_Streamlit_apps/GWP_Well_capture/pages/03_👉_About.py
@@ -7,8 +7,20 @@ st.markdown(
     The WellCapture interactive tool calculates the capture area for a pumping well in a confined, homogeneous, isotropic aquifer.
     
     ### Development
-    WellCapture was developed by Thomas Reimann as a Jupyter notebook and adapted to the Groundwater Project interactive education format. It was released in October of 2024. All Groundwater Project books are available for free download.
+    Innovative and digital learning and teaching materials are currently enhanced and transferred to various partners across Europe by the EU cooperation project iNUX. The iNUX project is funded by the ERASMUS+ program of the European Union.
     
+    WellCapture was developed by Thomas Reimann as a Jupyter notebook and adapted to the Groundwater Project interactive education format. It was released in October of 2024. All Groundwater Project books are available for free download.
+    """
+)
+
+left_co2, cent_co2 = st.columns((20,60))
+with left_co2:
+    st.image('90_Streamlit_apps/GWP_Well_capture/assets/images/iNUX_wLogo.png')
+with cent_co2:
+    st.image('90_Streamlit_apps/GWP_Well_capture/assets/images/1200px-Erasmus+_Logo.svg.png')
+
+st.markdown(
+    """
     ### About the Developer
 """
 )
@@ -23,15 +35,5 @@ with cent_co:
         Thomas is a specialist in applying and adapting distributed numerical models for use in research and industry. He enhanced the distributed numerical discrete-continuum model MODFLOW Conduit Flow Process (CFP) by various boundary conditions, flow- and transport processes as CFPv2. Current research projects comprise Karst system characterization with inverse groundwater modeling, groundwater management in open-pit mining environments, and Managed Aquifer Recharge.
         
         Besides research, he has been actively teaching groundwater management and groundwater modeling since 2003 at TU Dresden and as a guest lecturer for Hydrogeology since 2017 at the University of Gothenburg (Sweden). The ongoing teaching activities use various innovative digital methods to improve the learning process, which was honored by the TU Dresden teaching award in 2017.
-    """
+        """
     )
-st.markdown(
-    """
-    Innovative and digital learning and teaching materials are currently enhanced and transferred to various partners across Europe by the EU cooperation project iNUX.The iNUX project is funded by the ERASMUS+ program of the European Union.Logo of the iNUX project
-"""
-)
-left_co2, cent_co2 = st.columns((20,60))
-with left_co2:
-    st.image('90_Streamlit_apps/GWP_Well_capture/assets/images/iNUX_wLogo.png')
-with cent_co2:
-    st.image('90_Streamlit_apps/GWP_Well_capture/assets/images/1200px-Erasmus+_Logo.svg.png')
