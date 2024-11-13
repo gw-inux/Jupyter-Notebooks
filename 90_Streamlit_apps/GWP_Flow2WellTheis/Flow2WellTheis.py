37d01faadeb1a252b2591bd7ba23b13c5461691c
ThoReimann <Thomas.Reimann@tu-dresden.de>
2024-11-13 18:51:05 +0100

    Added Flow2Well Theis

---
+++ 90_Streamlit_apps/GWP_Flow2WellTheis/Flow2WellTheis.py
@@ -0,0 +1,43 @@
+import streamlit as st
+
+
+st.set_page_config(
+    page_title="Flow2Well Theis",
+    page_icon="💦",
+)
+
+st.write("# Flow2Well - Transient Theis App! 💦")
+
+st.sidebar.success("☝️ Select a page above. ☝️")
+
+st.header('Welcome 👋')
+
+st.markdown(
+    """
+    This app is designed to introduce the computation of transient flow to a well in a confined aquifer. 
+     
+    [Motivation / reason] with the following properties entered by the user.
+    - Transmissivity _T_ (square meters per second)
+    - Storativity _S_
+    - pumping rate _Q_ (cubic meters per second)
+    
+    Additional inputs allow the user to reformat the graph ...
+"""
+)
+
+left_co, cent_co, last_co = st.columns((20,60,20))
+with cent_co:
+    st.image('90_Streamlit_apps/GWP_Flow2WellTheis/assets/images/Flow_well_confined.png', caption="Sketch of the developing cone of drawdown for abstraction wells; modified from YX(XZY)")
+
+st.markdown(
+    """   
+    To navigate the Flow2Well tool you can use menu items at the sidebar:
+    - Learn about the underlying theory
+    - Investigate how a confined aquifer reacts on pumping
+    - Estimate the parameters of an aquifer by parameter fitting
+    - Estimate the parameters of real aquifers by example data sets
+    - Finally, compare the reaction of confined and unconfined aquifers on pumping.
+    
+    The online version of Flow2Well is copyrighted by the author and distributed by The Groundwater Project. Please use gw-project.org links when you want to share Groundwater Project materials with others. It is not permissible to make GW-Project documents available on other websites nor to send copies of the files directly to others
+"""
+)
---
+++ 90_Streamlit_apps/GWP_Flow2WellTheis/pages/01_📃_Theory.py
@@ -0,0 +1,43 @@
+import streamlit as st
+
+st.title('📃 Theory underlying Transient Flow to Wells - Theis solution')
+
+st.markdown("""
+            #### This variant of the app allows to choose randomly generated data with measurement noise
+            This interactive document allows to apply the Theis principle for pumping test evaluation in confined, transient setups. The notebook is based on an Spreadsheet from Prof. Rudolf Liedl.
+            
+            ### General situation
+            We consider a confined aquifer with constant transmissivity. If a well is pumping water out of the aquifer, radial flow towards the well is induced. To calculate the hydraulic situation, the following simplified flow equation can be used. This equation accounts for 1D radial transient flow towards a fully penetrating well within a confined aquifer without further sinks and sources:
+"""
+)
+st.latex(r'''\frac{\partial^2 h}{\partial r^2}+\frac{1}{r}\frac{\partial h}{\partial r}=\frac{S}{T}\frac{\partial h}{\partial t}''')
+st.markdown("""
+            ### Mathematical model and solution
+            Charles V. Theis presented a solution for this by deriving
+"""
+)
+st.latex(r'''s(r,t)=\frac{Q}{4\pi T}W(u)''')
+st.markdown("""
+            with the well function
+"""
+)
+st.latex(r'''W(u) = \int_{u }^{+\infty} \frac{e^{-\tilde u}}{\tilde u}d\tilde u''')
+st.markdown("""
+            and the dimensionless variable
+"""
+)
+st.latex(r'''u = \frac{Sr^2}{4Tt}''')
+st.markdown("""
+            This equations are not easy to solve. Historically, values for the well function were provided by tables or as so called type-curve. The type-curve matching with experimental data for pumping test analysis can be considered as one of the basic hydrogeological methods. However, modern computer provide an easier and more convinient way to solve the 1D radial flow equation based on the Theis approach. Subsequently, the Theis equation is solved with Python routines.
+"""
+)            
+
+
+columnsN1 = st.columns((1,1,1), gap = 'large')
+with columnsN1[0]:
+    st.write()
+with columnsN1[1]:
+    st.subheader(':orange[**Navigation**]')
+with columnsN1[2]:
+    if st.button("Next page"):
+        st.switch_page("pages/02_📈_▶️ Transient_Flow to a Well.py")
