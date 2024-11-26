01013abb0cd96ca29d879578787f6a5cd3ef9cf3
gmerritt123 <h2merrit@gmail.com>
2024-11-25 20:27:31 -0500

    Added annotations for width and x0, convert to km. Headed for build.

--- 90_Streamlit_apps/GWP_Well_capture/pages/cb.mjs
+++ 90_Streamlit_apps/GWP_Well_capture/pages/cb.mjs
@@ -8,25 +8,23 @@ function makeArr(startValue, stopValue, cardinality) {
   return arr;
 }
 
-// import * as d3 from "https://cdnjs.cloudflare.com/ajax/libs/d3/7.9.0/d3.min.js"
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9/+esm"
 
-
-export default function({src,sl_dict,f}){
+export default function({src,sl_dict,f,w_src,w_lbl,c_src,c_lbl}){
 	//collect slider values
-	var q = sl_dict['Pumping'].value
-	var k = 10**sl_dict['Conductivity'].value
+	var q = sl_dict['Pumping'].value/10**9 //cubic km/s 
+	var k = 10**sl_dict['Conductivity'].value/1000 //km/s
 	var i = 10**sl_dict['Gradient'].value
-	var b = sl_dict['Thickness'].value
+	var b = sl_dict['Thickness'].value/1000 //km
 
-	//get ymax_conf and x0_conf
+	//get ymax and x0_conf
 	var ymax = q/(2*k*i*b)
 	var x0 = q/(2*Math.PI*k*i*b)
 
 	//calculate array for y based on ymax
 	var ya = makeArr(-ymax*0.999,ymax*0.999, 1000)
 	//calculate array for x based on ya
-	var xa = ya.map(y=>y/(Math.tan(2*Math.PI*k*i*b*y/q)))
+	var xa = ya.map(y=>y/(Math.tan(2*Math.PI*k*i*b*y/q))) //convert m to km
 
 	//update src
 	src.data = {'x':xa, 'y':ya}
@@ -38,4 +36,21 @@ export default function({src,sl_dict,f}){
 	var yr = (f.x_range.end - f.x_range.start)/2
 	f.y_range.start = -yr
 	f.y_range.end = yr
+	
+	//calculate max width
+	var w = q/(k*i*b)
+	
+	//update max width annotation
+	w_src.data={'x0':[x0*-22],'x1':[x0*-22],'y0':[-w/2], 'y1':[w/2]}
+    w_lbl.visible = true
+    w_lbl.text = 'Max Width = '+w.toFixed(2).toString()+' km'
+    w_lbl.x = x0*-21.25
+    
+    //update culm pt annotation
+    c_src.data={'x0':[0],'x1':[x0],'y0':[0], 'y1':[0]}
+    c_lbl.visible = true
+    c_lbl.text = 'X₀ =\n'+x0.toFixed(2).toString()+' km'
+    c_lbl.x = -x0*1.7
+    c_lbl.y = w*0.05
+          	
 }
