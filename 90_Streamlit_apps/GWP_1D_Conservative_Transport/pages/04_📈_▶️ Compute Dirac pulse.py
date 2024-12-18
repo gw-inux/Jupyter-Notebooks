c9e8b33034cd12dfafda38e3ce626c1af090f709
ThoReimann <Thomas.Reimann@tu-dresden.de>
2024-12-18 14:17:12 +0100

    Update 04_📈_▶️ Compute Dirac pulse.py

--- 90_Streamlit_apps/GWP_1D_Conservative_Transport/pages/04_📈_▶️ Compute Dirac pulse.py
+++ 90_Streamlit_apps/GWP_1D_Conservative_Transport/pages/04_📈_▶️ Compute Dirac pulse.py
@@ -98,7 +98,7 @@ for t in range(t0, t1, dt):
     time.append(t)
     
 #compute concentration profile
-for xp in range(0, 100, 1):      
+for xp in range(0, 100, 0.1):      
     # ADVECTION-DISPERSION
     cp = ci+c_ADE(xp, tp, dM, Area, n, a, v)
     concp.append(cp)
