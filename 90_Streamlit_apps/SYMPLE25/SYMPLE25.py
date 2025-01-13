97c7acb9ff64f5dd7fbd421cf1ed17e1fcf5c9e4
ThoReimann <Thomas.Reimann@tu-dresden.de>
2025-01-13 11:11:39 +0100

    Update SYMPLE25.py

--- 90_Streamlit_apps/SYMPLE25/SYMPLE25.py
+++ 90_Streamlit_apps/SYMPLE25/SYMPLE25.py
@@ -18,7 +18,8 @@ well_capture_OM = st.Page("pages/00_OM/WellCapture.py", title="Initial Model: �
 
 #M1A - Basics
 f2w_conf_unconf             = st.Page("pages/M1A/Flow2Well_transient_unconfined_confined_basics.py", title="Flow2Well: 📈 1D radial flow to wells")
-transport_1D_basics         = st.Page("pages/M1A/Transport_1D_AD_basics.py", title="Types of movement: 📈 Example of 1D transport")gwf_1D_unconf_basics        = st.Page("pages/M1A/GWF_1D_unconf_analytic_noflow_calib_basics.py", title="Regional Hydrology: 📈 Example of 1D flow")
+transport_1D_basics         = st.Page("pages/M1A/Transport_1D_AD_basics.py", title="Types of movement: 📈 Example of 1D transport")
+gwf_1D_unconf_basics        = st.Page("pages/M1A/GWF_1D_unconf_analytic_noflow_calib_basics.py", title="Regional Hydrology: 📈 Example of 1D flow")
 radio_decay                 = st.Page("pages/M1A/Radioactive_Decay_basics.py", title="Radioactive Decay: 📈 Example of a mass balance")
 
 # M1C - Flow modeling
