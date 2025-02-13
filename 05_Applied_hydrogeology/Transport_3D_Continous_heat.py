710fdedae52e47f971d167ad5154f0da767e27e3
ThoReimann <Thomas.Reimann@tu-dresden.de>
2025-02-13 19:36:42 +0100

    Update Transport_3D_Continous_heat.py

--- 05_Applied_hydrogeology/Transport_3D_Continous_heat.py
+++ 05_Applied_hydrogeology/Transport_3D_Continous_heat.py
@@ -1,4 +1,4 @@
-import matplotlib
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 from matplotlib import cm, ticker
 from scipy import special
@@ -7,15 +7,15 @@ import streamlit as st
 from streamlit_extras.stateful_button import button
 
 st.title('3D :red-background[Heat transport] with convection, conduction, and dispersion')
-st.subheader('Heat input as :orange[Continous injection]', divider="orange")
+
+st.subheader('Heat input as :orange[continous injection]', divider="orange")
 
 columns1 = st.columns((1,1,1), gap = 'large')
 with columns1[1]:
     theory = button('Show/Hide theory', key = 'button1')
     
 if theory:
-#    st.latex(r'''C(x = vt + X, y = Y, z = Z) = \frac{M}{8 (\pi t)^{\frac{3}{2}} \sqrt{D_x D_y D_z}} \exp ( - ( \frac{X^2}{4 D_x t} + \frac{Y^2}{4 D_y t} + \frac{Z^2}{4 D_z t})''')
-     st.latex(r""" H(x,y,z,t) = \frac{C_0}{8}\left(\operatorname{erfc}\left( \frac{x - v_x t}{2\sqrt{D_x t}} \right) \left( \operatorname{erf} \left( \frac{y + \frac{Y}{2}}{2 \sqrt{D_y \frac{x}{v}}} \right)  - \operatorname{erf} \left(\frac{y - \frac{Y}{2}}{2 \sqrt{D_y \frac{x}{v}}} \right)\right)\left(\operatorname{erf} \left( \frac{z + \frac{Z}{2}}{2 \sqrt{D_z \frac{x}{v}}} \right)-\operatorname{erf} \left( \frac{z - \frac{Z}{2}}{2 \sqrt{D_z \frac{x}{v}}}\right)\right)\right)""")
+     st.latex(r""" H(x,y,z,t) = \frac{T_0}{8}\left(\operatorname{erfc}\left( \frac{x - v_x t}{2\sqrt{D_x t}} \right) \left( \operatorname{erf} \left( \frac{y + \frac{Y}{2}}{2 \sqrt{D_y \frac{x}{v}}} \right)  - \operatorname{erf} \left(\frac{y - \frac{Y}{2}}{2 \sqrt{D_y \frac{x}{v}}} \right)\right)\left(\operatorname{erf} \left( \frac{z + \frac{Z}{2}}{2 \sqrt{D_z \frac{x}{v}}} \right)-\operatorname{erf} \left( \frac{z - \frac{Z}{2}}{2 \sqrt{D_z \frac{x}{v}}}\right)\right)\right)""")
 
 '---'
 
@@ -77,23 +77,28 @@ T_ini    = 10
 columns1 = st.columns((1,1,1), gap = 'large')
 
 with columns1[0]:
-    TB = st.slider(f'**Source temperature (°C)**',1,100,T_ini+10,1) 
-    Y  = st.slider(f'**Source width (m)**',1,100,10,1)   
-    Z  = st.slider(f'**Source thickness (m)**',1,20,2,1) 
-    q  = st.slider(f'**specific discharge (m/s)**',0.001,0.1,0.016,0.001,format='%5.3f') 
+    with st.expander("Here you find widgets to :red[**adjust the inlet boundary**]"): 
+        TB = st.slider(f'**Source temperature (°C)**',1,100,T_ini+10,1) 
+        Y  = st.slider(f'**Source width (m)**',1,100,10,1)   
+        Z  = st.slider(f'**Source thickness (m)**',1,20,2,1) 
+    qd = st.slider(f'**specific discharge (m/d)**',0.001,1.0,0.25,0.001,format='%5.3f') 
+    q = qd/86400
     
 with columns1[1]:
-    n_e = st.slider(f'**effective Porosity (-)**',0.01,0.6,0.25,0.01)    
-    ax = st.slider(f'**Longitudinal (x) dispersivity (m)**',0.001,10.0,1.00,0.001)
-    rat_x_y = st.slider(f'**Dispersivity ratio 1/n for x/y**',1,100,10,1)
-    rat_x_z = st.slider(f'**Dispersivity ratio 1/n for x/z**',1,500,100,1)
+    with st.expander("Here you find widgets to :orange[**adjust dispersion**]"):    
+        ax = st.slider(f'**Longitudinal (x) dispersivity (m)**',0.001,10.0,1.00,0.001)
+        rat_x_y = st.slider(f'**Dispersivity ratio 1/n for x/y**',1,100,10,1)
+        rat_x_z = st.slider(f'**Dispersivity ratio 1/n for x/z**',1,500,100,1)
+    n_e = st.slider(f'**effective Porosity (-)**',0.01,0.6,0.25,0.01)  
 with columns1[2]:
-    isolines = st.toggle("Show isolines instead of isosurfaces")
-    down_only = st.toggle("Spreading is only downward")
-    Zp = st.slider(f'**Slice at z (top view plot)**',-25,25,0,1)
-    Yp = st.slider(f'**Slice at y (side view plot)**',-100,100,0,1)
-    xmax = st.slider(f'**Extension in x-direction**',0,1000,400,50)
-    t = st.slider(f'**Time for the temperature profile (s)**',1.,1800.,1.,1.)
+    with st.expander("Here you find widgets to :green[**adjust the plot**]"):
+        isolines = st.toggle("Show isolines instead of isosurfaces")
+        down_only = st.toggle("Spreading is only downward")
+        Zp = st.slider(f'**Slice at z (top view plot)**',-25,25,0,1)
+        Yp = st.slider(f'**Slice at y (side view plot)**',-100,100,0,1)
+        xmax = st.slider(f'**Extension in x-direction**',0,1000,200,50)
+    td = st.slider(f'**Time to plot (d)**',1.,1800.,183.,1.)
+    t = td * 86400
     
 T0 = TB - T_ini
 ay = ax/rat_x_y
@@ -102,7 +107,7 @@ az = ax/rat_x_z
 # Grid for X and Y
 # xmax = 500
 ymax = xmax/10
-zmax = 40
+zmax = xmax/25
 x_vals = np.linspace(-0.1*xmax, xmax, 300)
 y_vals = np.linspace(-ymax, ymax, 300)
 z_vals = np.linspace(-zmax, zmax, 300)
@@ -121,7 +126,7 @@ Txz[np.isnan(Txz)] = T_ini
 lev_exp = 10.**np.arange(-8, 3)
 
 fig = plt.figure(figsize=(16,8))
-gs = matplotlib.gridspec.GridSpec(3,2, width_ratios=[8,1.1], height_ratios=[5,0.2,2])
+gs = mpl.gridspec.GridSpec(3,2, width_ratios=[8,1.1], height_ratios=[5,0.2,2])
 
 ax = fig.add_subplot(gs[0,:])
 if isolines:
@@ -131,6 +136,10 @@ else:
     #contour = plt.contourf(xxy, yxy, Txy, lev_exp, locator=ticker.LogLocator())
     contour = plt.contourf(xxy, yxy, Txy)
 ax.vlines(0, -Y/2, Y/2, linewidth = 10, color='fuchsia', label='Source of heat')
+
+cmap = mpl.cm.cool
+norm = mpl.colors.Normalize(vmin=T_ini, vmax=TB)
+
 plt.colorbar(contour, label="Temperature (degree Celsius)", format='%5.2f')
 plt.xlabel("x in m",fontsize=14)
 plt.ylabel("y in m",fontsize=14)
