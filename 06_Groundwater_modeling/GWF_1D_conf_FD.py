50a03ece1b22dbe843d1fdacae64e4b22185dd05
ThoReimann <Thomas.Reimann@tu-dresden.de>
2025-02-20 12:54:34 +0100

    updates

--- 06_Groundwater_modeling/GWF_1D_conf_FD.py
+++ 06_Groundwater_modeling/GWF_1D_conf_FD.py
@@ -8,9 +8,21 @@ import pandas as pd
 from IPython.display import clear_output
 import math
 import streamlit as st
+from streamlit_extras.stateful_button import button
 
 st.title('Finite-Difference Numerical scheme')
 st.subheader('1D groundwater flow in a confined aquifer with uniform recharge', divider='blue')
+
+st.markdown("""
+            ### Introduction and Motivation
+            Groundwater models are useful tools to evaluate groundwater systems. This app demonstrate a simple 1D numerical scheme to simulate groundwater flow in a 1D confined aquifer that is bounded by two defined head boundaries.
+           """)
+
+lc0, cc0, rc0 = st.columns((20,60,20))
+with cc0:
+    st.image('06_Groundwater_modeling/FIGS/GWF_1D_FD.png', caption="Schematic representation of the conceptual model and the numerical representation.")
+    
+"---"
 
 st.subheader('Input values')
 
@@ -21,29 +33,32 @@ st.markdown('''
 # Data input
 log_min1 = -7.0 # T / Corresponds to 10^-7 = 0.0000001
 log_max1 = 0.0  # T / Corresponds to 10^0 = 1
-column = st.columns((1,1), gap = 'large')
+column = st.columns((1,1,1))
 with column[0]:
-    st.write('**Parameter for the scenario**')
-    m       = st.slider('Aquifer thickness (in m)', 1, 200, 20, 1)
-    BC_L    = st.number_input('Left boundary head (in m)', 0.1, 500., 18., 0.1)
-    BC_R    = st.number_input('Right boundary head (in m)', 0.1, 500., 16., 0.1)
-    RCH_IN  = st.slider('Recharge (in mm/a)', 0, 500, 200, 1)
-    K_slider_value=st.slider('(log of) **hydraulic conductivity** in m/s', log_min1,log_max1,-3.0,0.01,format="%4.2f" )
-    # Convert the slider value to the logarithmic scale
-    K = 10 ** K_slider_value
-    # Display the logarithmic value
-    st.write("_hydraulic conductivity_ in m/s: %5.2e" %K)
+    with st.expander('**Parameter for the scenario**'):
+        m       = st.slider('Aquifer thickness (in m)', 1, 200, 20, 1)
+        BC_L    = st.number_input('Left boundary head (in m)', 0.1, 500., 18., 0.1)
+        BC_R    = st.number_input('Right boundary head (in m)', 0.1, 500., 16., 0.1)
+        RCH_IN  = st.slider('Recharge (in mm/a)', 0, 500, 200, 1)
+        container = st.container()
+        K_slider_value=st.slider('_(log of) **hydraulic conductivity_ in m/s', log_min1,log_max1,-3.0,0.01,format="%4.2f" )
+        # Convert the slider value to the logarithmic scale
+        K = 10 ** K_slider_value
+        # Display the logarithmic value
+        container.write("**Hydraulic conductivity** in m/s: %5.2e" %K)
+    st.session_state.analytic = st.toggle("Show analytic solution", help="Press here to plot the analytical solution for comparison") 
 with column[1]:
-    st.write('**Parameter for the numerical solution**')
-    cells   = st.number_input('Number of cells', 3, 100, 11, 1)
-    dx      = st.number_input('Spatial increment dx (in m)',1,1000,500,1)   
-    st.session_state.i_max   = st.number_input('Max number of iterations', 5, 500, 50, 1)    
-    st.session_state.epsilon = st.number_input('Closure criteria (in m)', 0.00001, 10., 0.001, 0.00001, format="%0.4f")
-    st.session_state.analytic = st.toggle("Show analytic solution", help="Press here to start the iteration with the analytic solution")    
+    with st.expander('**Spatial discretization**'):
+        cells   = st.number_input('Number of cells', 3, 100, 11, 1)
+        dx      = st.number_input('Spatial increment dx (in m)',1,1000,500,1) 
+with column[2]:
+    with st.expander('**Parameter for the numerical solution**'):  
+        st.session_state.i_max   = st.number_input('Max number of iterations', 5, 500, 50, 1)    
+        st.session_state.epsilon = st.number_input('Closure criteria (in m)', 0.00001, 10., 0.001, 0.00001, format="%0.4f")
+        st.session_state.analytic = st.toggle("Show analytic solution", help="Press here to start the iteration with the analytic solution")    
 
 i = 0
 run = False
-
     
 # Länge der x-Achse (Achtung, das numerische Verfahren ist Knotenzentriert, d. h. x = 0 in der Mitte der ersten Zelle)
 L = (cells-1) * dx
@@ -110,7 +125,11 @@ with empty.container():
 
 @st.fragment
 def computation():
-    run = st.button("Run the computation", help="Press here to start the iteration")
+    
+    lc2, mc2, rc2 = st.columns([1,1,1])
+    with mc2:
+        run = st.button("**:blue[Run the computation]**", help="Press here to start the iteration")
+    
     i = 0
     convergence = False
     if run:
@@ -158,11 +177,11 @@ def computation():
                                
 # Abbruchkriterium der Iterationsschleife
             if convergence:       # Abbruch der Iteration
-                st.write('Convergence achieved')
+                st.write(':green[Convergence achieved]')
                 break
         
     # If no convergence
     if convergence == False:        
-        st.write('NO CONVERGENCE YET')
+        st.write(':red[NO CONVERGENCE YET]')
 
 computation()
