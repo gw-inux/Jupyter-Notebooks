069da3f04d3ac89901abc7c61a8a88c547b600c3
ThoReimann <Thomas.Reimann@tu-dresden.de>
2025-03-24 22:02:52 +0100

    Update Q_h_plot_Cauchy.py

--- 06_Groundwater_modeling/Q_h_plot_Cauchy.py
+++ 06_Groundwater_modeling/Q_h_plot_Cauchy.py
@@ -3,14 +3,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 st.title("Interaction Between Groundwater and Surface Water")
+st.subheader("Theory and Concept of River Aquifer Interaction", divider="green")
 
 st.markdown("""
 This app shows how the flow between a stream and an aquifer $Q$ depends on the groundwater head at the stream $h_{aq}$. 
-The relationship follows:  
-$
-Q = C \\cdot (h_{RIV} - h_{{aq}})
-$
+The relationship follows:
 """)
+
+st.latex(r'''Q_{RIV} = C (h_{RIV} - h_{{aq}})''')
+
+st.markdown("""
+where:
+- $Q_{RIV}$ is the flow between the river and the aquifer, taken as positive if it is directed into the aquifer [L3/T]
+- $h_{RIV}$ is the water level (stage) in the river (L),
+- $C_{RIV}$ is the hydraulic conductance of the river-aquifer interconnection [L2/T], and
+- $h_{aq}$ is the head in the aquifer that interacts with the river (L).
+
+In case the aquifer head $h_{aq}$ falls below the river bottom elevation $R_{BOT}$, the relationship is:
+""")
+
+st.latex(r'''Q_{RIV} = C (h_{RIV} - R_{{BOT}})''')
+
+st.subheader("Interactive plot", divider="green")
 
 # Main area inputs
 
@@ -30,7 +44,7 @@ with columns1[0]:
     container.write("**Conductance in m²/s:** %5.2e" %C)
 with columns1[1]:
     h_RIV = st.slider("**River head ($h_{RIV}$)**", min_value=5.0, max_value=20.0, value=9.0, step=0.1)
-    h_aq_show  = st.slider("**Aquifer head ($h_{aq}**$) for visualization", min_value=0.0, max_value=20.0, value=10.0, step=0.1)
+    h_aq_show  = st.slider("**Aquifer head ($h_{aq}$**)", min_value=0.0, max_value=20.0, value=10.0, step=0.1)
     if bottom:
         stage = st.slider("**River stage ($h_{stage}$)**", min_value=0.1, max_value=5.0, value=2.0, step=0.1)
         h_bot = h_RIV-stage
@@ -50,7 +64,7 @@ ax.axvline(h_RIV, color='gray', linestyle='--', label=f'$h_{{RIV}}$ = {h_RIV}')
 ax.axvline(h_aq_show, color='red', linestyle='--', label=f'$h_{{aq}}$ = {h_aq_show}')
 
 # Labels and formatting
-ax.set_xlabel("Head in the Ground-Water System at the Stream ($h_{aq}$)", fontsize=10)
+ax.set_xlabel("Hydraulic heads and elevations in the River-Aquifer System", fontsize=10)
 ax.set_ylabel("Flow Into the Ground-Water System From the Stream ($Q$)", fontsize=10)
 ax.set_title("Flow Between Groundwater and Stream", fontsize=12)
 ax.set_xlim(0, 20)
