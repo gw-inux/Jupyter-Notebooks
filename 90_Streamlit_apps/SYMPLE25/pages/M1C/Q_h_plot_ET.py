610f4ac15ad39d43820b993cd78e19c2fa08233d
ThoReimann <Thomas.Reimann@tu-dresden.de>
2025-04-01 17:13:42 +0200

    Modified the Symple Multipage app, added boundaries

---
+++ 90_Streamlit_apps/SYMPLE25/pages/M1C/Q_h_plot_ET.py
@@ -0,0 +1,194 @@
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+
+# TODO:
+# - allow user to plot rate / discharge 
+# - adjust plot for discharge according to the max (rate x area)
+
+# Authors, institutions, and year
+year = 2025 
+authors = {
+    "Thomas Reimann": [1],  # Author 1 belongs to Institution 1
+}
+institutions = {
+    1: "TU Dresden, Institute for Groundwater Management",
+}
+index_symbols = ["¹", "²", "³", "⁴", "⁵", "⁶", "⁷", "⁸", "⁹"]
+author_list = [f"{name}{''.join(index_symbols[i-1] for i in indices)}" for name, indices in authors.items()]
+institution_list = [f"{index_symbols[i-1]} {inst}" for i, inst in institutions.items()]
+institution_text = " | ".join(institution_list)
+
+st.title("Consideration of Evapotranspiration on Groundwater Ressources")
+st.subheader("Theory and Concept of Evapotranspiration in MODFLOW", divider="green")
+
+st.markdown("""
+This app shows the effect of evapotranspiration in removing water from an aquifer according to Harbaugh (2005) as it is implemented by the EVT package in MODFLOW. The EVT package consider only evapotranspiration from the saturated zone. The approach considers the following parameters/measures:
+- $EVTR$ = a user defined maximum evapotranspiration rate,
+- :green[$SURF$ = a specific elevation named _ET surface_ up to which the full evapotranspiration rate is acting],
+- $RET$ = the depth-specific evapotranspiration rate,
+- :orange[$EXDP$ = _extinction depth_ or cutoff depth ($EXDP$), where the evapotranspiration rate $ETR$ from the groundwater becomes zero.]
+
+The approach assumes:
+- if heads are above the _ET surface_ ($SURF$), the evapotranspiration rate $RET$ is the user-defined maximum rate $EVTR$
+""")
+st.latex(r'''\text{RET} = \text{EVTR}, \quad h_{i,j,k} > \text{SURF}''')
+st.markdown("""
+- if heads are below the _ET surface_ ($SURF$) and exceeding the _extinction depth_ or cutoff depth ($EXDP$), the evapotranspiration rate $ETR$ from the groundwater becomes zero.
+""")
+st.latex(r'''\text{RET} = 0, \quad h_{i,j,k} < \text{SURF} - \text{EXDP}''')
+st.markdown("""
+- between these two thresholds, evapotranspiration increase linearly from the _extinction depth_ to the _ET surface_.
+""")
+st.latex(r'''\text{RET} = \text{EVTR} \frac{h_{i,j,k} - (\text{SURF} - \text{EXDP})}{\text{EXDP}}, \quad (\text{SURF} - \text{EXDP}) \leq h_{i,j,k} \leq \text{SURF}
+''')
+st.markdown("""
+The volumetric discharge is computed by multiplying the evapotranspiration rate $RET$ by the cell area $\Delta x \Delta y$:
+""")
+st.latex(r'''Q_{ET} = RET \Delta x \Delta y''')
+
+
+
+st.subheader("Interactive plot", divider="green")
+st.markdown("""
+The interactive plot allows you to investigate $Q_{ET}$ in dependence from the _ET surface_ and the _extinction depth_. You can turn the plot by using the toggle above the input widgets.
+""")
+
+# Functions
+
+# Callback function to update session state
+#SURF
+#EXDP
+#EVTR_input
+def update_SURF():
+    st.session_state.SURF = st.session_state.SURF_input
+def update_EXDP():
+    st.session_state.EXDP = st.session_state.EXDP_input
+def update_EVTR_input():
+    st.session_state.EVTR_input = st.session_state.EVTR_input_input
+    
+# Initialize session state for value and toggle state
+st.session_state.SURF = -1.0
+st.session_state.EXDP = 4.0
+st.session_state.EVTR_input = 2.0
+
+st.session_state.number_input = False  # Default to number_input
+
+# Main area inputs
+@st.fragment
+def Q_h_plot():
+
+    turn = st.toggle('Toggle to turn the plot 90 degrees')
+    st.session_state.number_input = st.toggle("Toggle to use Slider or Number for input of $SURF$, $EXDP$, and $EVTR$.")
+    
+    columns1 = st.columns((1,1), gap = 'large')
+    with columns1[0]:
+        if st.session_state.number_input:
+            SURF = st.number_input("**ET surface $SURF$**", -3.0, 0.0, st.session_state.SURF, 0.1,key="SURF_input", on_change=update_SURF)
+            EXDP = st.number_input("**Extinction depth $EXDP$**", 0.1, 5.0, st.session_state.EXDP, 0.1,key="EXDP_input", on_change=update_EXDP)
+        else:
+            SURF = st.slider("**ET surface $SURF$**", -3.0, 0.0, st.session_state.SURF, 0.1,key="SURF_input", on_change=update_SURF)
+            EXDP = st.slider("**Extinction depth $EXDP$**", 0.1, 5.0, st.session_state.EXDP, 0.1,key="EXDP_input", on_change=update_EXDP)
+    
+    with columns1[1]:
+        if st.session_state.number_input:
+            EVTR_input = st.number_input("**Maximum Evapotranspiration rate ($EVTR$ in mm/d)**", 0.1, 20.0, st.session_state.EVTR_input, 0.1,key="EVTR_input_input", on_change=update_EVTR_input)
+        else:
+            EVTR_input = st.slider("**Maximum Evapotranspiration rate ($EVTR$ in mm/d)**", 0.1, 20.0, st.session_state.EVTR_input, 0.1,key="EVTR_input_input", on_change=update_EVTR_input)
+        EVTR = EVTR_input/86400000
+        AREA = st.number_input("**Cell Area ($\\Delta x \\Delta y$) in m2**", min_value=1.0, max_value=40000.0, value=10000.0, step=100.0)
+    
+    QET_MAX = 0.0005
+    
+    # Define aquifer head range
+    h_aq = np.linspace(-10, 0, 100)
+    
+    RET = np.where(h_aq > SURF, EVTR, np.where(h_aq >= (SURF - EXDP), EVTR * (h_aq - (SURF - EXDP)) / EXDP, 0))
+    
+    QET = RET*AREA
+    
+    # Create the plot
+    fig, ax = plt.subplots(figsize=(6, 6))
+    if turn:
+        ax.plot(QET, h_aq, label="$Q_{ET}$",color='blue', linewidth=3)
+        ax.axhline(0, color='black', linewidth=5)
+        ax.axhline(SURF, color='green', linestyle='--', label=f'$SURF$ in m a.s.l.= {SURF:5.2f}')
+        ax.axhline((SURF-EXDP), color='red', linestyle='--', label=f'$(SURF-EXDP)$ in m = {(SURF-EXDP):5.2f}')
+        arrow_x = 0.8 * QET_MAX  # horizontal position
+        arrow_y_start = SURF
+        arrow_y_end = (SURF - EXDP)
+    
+        ax.annotate(
+            '', 
+            xy=(arrow_x, arrow_y_end),       # arrowhead
+            xytext=(arrow_x, arrow_y_start), # arrow base
+            arrowprops=dict(
+                arrowstyle='<->', color='orange', lw=3, alpha=0.7
+            )
+        )
+        ax.text(
+            arrow_x + 0.05 * QET_MAX,
+            (arrow_y_start + arrow_y_end) / 2,
+            "EXDP",
+            color='black',
+            fontsize=10,
+            ha='center'
+        )
+    else:
+        ax.plot(h_aq, QET, label="$Q_{ET}$",color='blue', linewidth=3)
+        ax.axvline(0, color='black', linewidth=5)
+        ax.axvline(SURF, color='green', linestyle='--', label=f'$SURF$ in m a.s.l.= {SURF:5.2f}')
+        ax.axvline((SURF-EXDP), color='red', linestyle='--', label=f'$(SURF-EXDP)$ in m = {(SURF-EXDP):5.2f}')
+        
+        arrow_y = 0.8 * QET_MAX  # horizontal position
+        arrow_x_start = SURF
+        arrow_x_end = (SURF - EXDP)
+    
+        ax.annotate(
+            '', 
+            xy=(arrow_x_end, arrow_y),       # arrowhead
+            xytext=(arrow_x_start, arrow_y), # arrow base
+            arrowprops=dict(
+                arrowstyle='<->', color='orange', lw=3, alpha=0.7
+            )
+        )
+        ax.text(
+            (arrow_x_start + arrow_x_end) / 2,
+            arrow_y + 0.02 * QET_MAX,  # slight vertical offset
+            "EXDP",
+            color='black',
+            fontsize=10,
+            ha='center'
+        )
+    
+    # Labels and formatting
+    if turn:
+        ax.set_ylabel("Heads in the aquifer system in m above sea level (a.s.l)", fontsize=10)
+        ax.set_xlabel("Evapotranspiration loss from the aquifer ($Q_{ET}$) in m³/s", fontsize=10)
+        ax.set_ylim(-10,0)
+        ax.set_xlim(-0.1*QET_MAX, QET_MAX)
+    else:
+        ax.set_xlabel("Heads in the aquifer system in m above sea level (a.s.l)", fontsize=10)
+        ax.set_ylabel("Evapotranspiration loss from the aquifer ($Q_{ET}$) in m³/s", fontsize=10)
+        ax.set_xlim(0,-10)
+        ax.set_ylim(-0.1*QET_MAX, QET_MAX)
+    ax.set_title("Evapotranspiration losses", fontsize=12)
+    
+    ax.grid(True)
+    ax.legend()
+    #ax.set_aspect('equal', adjustable='box')
+    
+    # Add gaining/losing stream annotations
+    #ax.text(0.2, -0.005, "Gaining Stream", va='center',color='blue')
+    #ax.text(0.2, 0.005, "Losing Stream", va='center',color='green')
+    
+    st.pyplot(fig)
+
+Q_h_plot()
+'---'
+# Render footer with authors, institutions, and license logo in a single line
+columns_lic = st.columns((5,1))
+with columns_lic[0]:
+    st.markdown(f'Developed by {", ".join(author_list)} ({year}). <br> {institution_text}', unsafe_allow_html=True)
+with columns_lic[1]:
+    st.image('FIGS/CC_BY-SA_icon.png')
---
+++ 90_Streamlit_apps/SYMPLE25/pages/M1C/Q_h_plot_GHB.py
@@ -0,0 +1,219 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.special
+import scipy.interpolate as interp
+import math
+import pandas as pd
+import streamlit as st
+import streamlit_book as stb
+from streamlit_extras.stateful_button import button
+
+# Authors, institutions, and year
+year = 2025 
+authors = {
+    "Thomas Reimann": [1],  # Author 1 belongs to Institution 1
+}
+institutions = {
+    1: "TU Dresden, Institute for Groundwater Management",
+}
+index_symbols = ["¹", "²", "³", "⁴", "⁵", "⁶", "⁷", "⁸", "⁹"]
+author_list = [f"{name}{''.join(index_symbols[i-1] for i in indices)}" for name, indices in authors.items()]
+institution_list = [f"{index_symbols[i-1]} {inst}" for i, inst in institutions.items()]
+institution_text = " | ".join(institution_list)
+
+st.title("Interaction Between Groundwater and Surface Water")
+st.subheader("Theory and Concept of the General Head Boundary (GHB) in MODFLOW", divider="green")
+
+st.markdown("""
+This app calculates the flow between a General Head Boundary (GHB) and a model cell depending on the boundary head $HB$ and the conductance $C_B$ between the boundary and the aquifer cell.
+ 
+The relationship follows:
+""")
+
+st.latex(r'''Q_B = C_B (H_B - h_{{aq}})''')
+
+st.markdown("""
+where:
+- $Q_B$ is the flow between the GHB and the aquifer, taken as positive if it is directed into the aquifer [L3/T]
+- $H_B$ is the water level (stage) in the general head boundary (L),
+- $C_B$ is the hydraulic conductance of the GHB-aquifer interconnection [L2/T], and
+- $h_{aq}$ is the head in the aquifer that interacts with the river (L).
+
+The following figure illustrates the setup.
+""")
+
+left_co, cent_co, last_co = st.columns((10,80,10))
+with cent_co:
+    st.image('06_Groundwater_modeling/FIGS/GHB.png', caption="Schematic illustration of the GHB boundary, modified from  (McDonald and Harbaugh, 1988; https://pubs.usgs.gov/twri/twri6a1/pdf/twri_6-A1_p.pdf)")
+
+st.subheader("Interactive plot", divider="green")
+
+# Functions
+
+# Callback function to update session state
+def update_C():
+    st.session_state.C_slider_value = st.session_state.C_input
+def update_K():
+    st.session_state.K_slider_value = st.session_state.K_input
+def update_LB():
+    st.session_state.LB = st.session_state.LB_input
+def update_AB():
+    st.session_state.AB = st.session_state.AB_input
+def update_HB():
+    st.session_state.HB = st.session_state.HB_input
+def update_stage():
+    st.session_state.stage = st.session_state.stage_input
+def update_h_aq_show():
+    st.session_state.h_aq_show = st.session_state.h_aq_show_input  
+    
+# Initialize session state for value and toggle state
+st.session_state.C_slider_value = -3.0
+st.session_state.K_slider_value = -4.0
+st.session_state.LB = 100.
+st.session_state.AB = 1000.0
+st.session_state.HB = 9.0
+st.session_state.stage = 2.0
+st.session_state.h_aq_show = 10.0
+
+st.session_state.number_input = False  # Default to number_input
+
+
+# Main area inputs
+@st.fragment
+def Q_h_plot():
+    
+    # Define the minimum and maximum for the logarithmic scale
+    log_min1 = -7.0 # T / Corresponds to 10^-7 = 0.0000001
+    log_max1 = 1.0  # T / Corresponds to 10^1 = 10
+    
+    # Switches
+    turn = st.toggle('Toggle to turn the plot 90 degrees')
+    st.session_state.number_input = st.toggle("Toggle to use Slider or Number for input of $C_B$, $H_B$, $A_B$, $L_B$, and $h_{aq}$.")
+    c_computed = st.toggle('Toggle to compute conductance')
+    
+    columns1 = st.columns((1,1), gap = 'large')
+    
+    # Initialize st.session_state.C
+    if "C" not in st.session_state:
+        st.session_state.C = 10 ** st.session_state.C_slider_value
+    
+    with columns1[0]:
+        # READ LOG VALUE, CONVERT, AND WRITE VALUE FOR TRANSMISSIVITY
+        if c_computed:
+            container = st.container()  
+            if st.session_state.number_input:
+                K_slider_value_new = st.number_input("_(log of) Hydraulic conductivity $K_B$ in m/s_", log_min1,log_max1, st.session_state.K_slider_value, 0.01, format="%4.2f", key="K_input", on_change=update_K)
+            else:
+                K_slider_value_new = st.slider      ("_(log of) Hydraulic conductivity $K_B$ in m/s_", log_min1,log_max1, st.session_state.K_slider_value, 0.01, format="%4.2f", key="K_input", on_change=update_K)   
+            K = 10 ** K_slider_value_new
+            container.write("**Hydraulic conductivity $K_B$ in m/s:** %5.2e" %K)
+            if st.session_state.number_input:
+                LB = st.number_input("**GHB lenght ($L_B$)**", 1.0, 10000.0, st.session_state.LB, 1., key="LB_input", on_change=update_LB)
+            else:
+                LB = st.slider      ("**GHB lenght ($L_B$)**", 1.0, 10000.0, st.session_state.LB, 1., key="LB_input", on_change=update_LB)
+            if st.session_state.number_input:
+                AB = st.number_input("**GHB area ($A_B$)**", 1.0, 100000.0, st.session_state.AB, 1., key="AB_input", on_change=update_AB)
+            else:
+                AB = st.slider      ("**GHB area ($A_B$)**", 1.0, 100000.0, st.session_state.AB, 1., key="AB_input", on_change=update_AB)
+            st.session_state.C = K * AB / LB
+            
+            # Update C_slider_value based on computed values
+            st.session_state.C_slider_value = np.log10(st.session_state.C)
+        else:
+            container = st.container()  
+            if st.session_state.number_input:
+                C_slider_value_new = st.number_input("_(log of) Conductance $C_B$ in m²/s_", log_min1,log_max1, st.session_state.C_slider_value, 0.01, format="%4.2f", key="C_input", on_change=update_C)
+            else:
+                C_slider_value_new = st.slider      ("_(log of) Conductance $C_B$ in m²/s_", log_min1,log_max1, st.session_state.C_slider_value, 0.01, format="%4.2f", key="C_input", on_change=update_C)    
+            st.session_state.C = 10 ** C_slider_value_new
+            container.write("**Conductance $C_B$ in m²/s:** %5.2e" %st.session_state.C)
+            
+            # Update K_slider_value based on computed values
+            st.session_state.K_slider_value = np.log10(st.session_state.C * st.session_state.LB / st.session_state.AB)
+    with columns1[1]:
+        if st.session_state.number_input:
+            HB = st.number_input("**GHB head ($HB$)**", 5.0, 20.0, st.session_state.HB, 0.1, key="HB_input", on_change=update_HB)
+        else:
+            HB = st.slider      ("**GHB head ($HB$)**", 5.0, 20.0, st.session_state.HB, 0.1, key="HB_input", on_change=update_HB)
+        if st.session_state.number_input:
+            h_aq_show = st.number_input("**Aquifer head ($h_{aq}$)**", 0.0, 20.0, st.session_state.h_aq_show, 0.1, key="h_aq_show_input", on_change=update_h_aq_show)
+        else:
+            h_aq_show = st.slider      ("**Aquifer head ($h_{aq})$**", 0.0, 20.0, st.session_state.h_aq_show, 0.1, key="h_aq_show_input", on_change=update_h_aq_show)
+    
+    # Define aquifer head range
+    h_aq = np.linspace(0, 20, 200)
+    Q = st.session_state.C * (HB - h_aq)
+    Q_ref = st.session_state.C * (HB - h_aq_show)
+    
+    # Create the plot
+    fig, ax = plt.subplots(figsize=(6, 6))
+    if turn:
+        ax.plot(Q, h_aq, label=rf"$Q_B = C_B(H_B - h_{{aq}})$, $C_B$ = {st.session_state.C:.2e}",color='orange', linewidth=3)
+        ax.axvline(0, color='black', linewidth=1)
+        ax.axhline(HB, color='green', linewidth=2, linestyle='--', label=f'$H_B$ in m= {HB}')
+        ax.axhline(h_aq_show, color='blue', linewidth=2, linestyle='--', label=f'$h_{{aq}}$ in m= {h_aq_show}')
+        # Labels and formatting
+        ax.set_ylabel("Heads and elevations in the GHB Boundary-Aquifer System (m)", fontsize=10)
+        ax.set_xlabel("Flow Into the Ground-Water System From the GHB $Q_B$ (m³/s)", fontsize=10)
+        ax.set_ylim(0, 20)
+        ax.set_xlim(0.05, -0.05)
+        if Q_ref < 0:
+            ax.annotate(
+                '',  # no text
+                xy=(Q_ref,HB),  # arrowhead
+                xytext=(Q_ref, h_aq_show),  # arrow start
+                arrowprops=dict(arrowstyle='->', color='blue', lw=3,  alpha=0.4)
+            )
+        else:
+            ax.annotate(
+                '',  # no text
+                xy=(Q_ref,HB),  # arrowhead
+                xytext=(Q_ref, h_aq_show),  # arrow start
+                arrowprops=dict(arrowstyle='<-', color='green', lw=3, alpha=0.6)
+            )
+        # Add gaining/losing stream annotations
+        ax.text(-0.003,1, "Gaining GHB boundary", va='center',color='blue')
+        ax.text(0.035, 1,  "Losing GHB boundary", va='center',color='green')
+            
+    else:
+        ax.plot(h_aq, Q, label=rf"$Q_B = C_B(H_B - h_{{aq}})$, $C_B$ = {st.session_state.C:.2e}",color='orange', linewidth=3)
+        ax.axhline(0, color='black', linewidth=1)
+        ax.axvline(HB, color='green', linewidth=2, linestyle='--', label=f'$H_B$ in m= {HB}')
+        ax.axvline(h_aq_show, color='blue', linewidth=2, linestyle='--', label=f'$h_{{aq}}$ in m= {h_aq_show}')
+        # Labels and formatting
+        ax.set_xlabel("Heads and elevations in the GHB Boundary-Aquifer System (m))", fontsize=10)
+        ax.set_ylabel("Flow Into the Ground-Water System From the GHB $Q_B$ (m³/s)", fontsize=10)
+        ax.set_xlim(0, 20)
+        ax.set_ylim(-0.05, 0.05)
+        if Q_ref < 0:
+            ax.annotate(
+                '',  # no text
+                xy=(HB, Q_ref),  # arrowhead
+                xytext=(h_aq_show, Q_ref),  # arrow start
+                arrowprops=dict(arrowstyle='->', color='blue', lw=3,  alpha=0.4)
+            )
+        else:
+            ax.annotate(
+            '',  # no text
+            xy=(HB, Q_ref),  # arrowhead
+            xytext=(h_aq_show, Q_ref),  # arrow start
+            arrowprops=dict(arrowstyle='<-', color='green', lw=3, alpha=0.6)
+            )
+        # Add gaining/losing stream annotations
+        ax.text(13, -0.003, "Gaining GHB boundary", va='center',color='blue')
+        ax.text(13, 0.003, "Losing GHB boundary", va='center',color='green')
+        
+    ax.set_title("Flow Between Groundwater and GHB boundary", fontsize=12)
+    ax.grid(True)
+    ax.legend()
+    
+    st.pyplot(fig)
+
+Q_h_plot()
+'---'
+# Render footer with authors, institutions, and license logo in a single line
+columns_lic = st.columns((5,1))
+with columns_lic[0]:
+    st.markdown(f'Developed by {", ".join(author_list)} ({year}). <br> {institution_text}', unsafe_allow_html=True)
+with columns_lic[1]:
+    st.image('FIGS/CC_BY-SA_icon.png')
