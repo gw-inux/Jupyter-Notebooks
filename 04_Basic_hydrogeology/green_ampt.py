2c1d429e3fa0903c01b86d672137aa34b4094da3
Steffen Birk <steffen.birk@uni-graz.at>
2025-11-11 10:49:00 +0100

    Update green_ampt.py

--- 04_Basic_hydrogeology/green_ampt.py
+++ 04_Basic_hydrogeology/green_ampt.py
@@ -67,12 +67,12 @@ with st.expander('Show variable descriptions'):
 
 
 with st.expander("Show typical parameter values"):
-        st.markdown(" *Table 1: Typical parameter values (after Chin, D.A.: Water-Resources Enginieering, 2013)."
-        "Please note that for the use in this app, the values of $K_s$ need to be transformed from mm/h to mm/min."
-        "The (field-)saturated water content $\theta_0$ can be approximated by value of porosity, " \
-        "the initial water content $\theta_i$ is likely to range between field capacity and wilting point, " \
-        "with the field capacitiy representing conditions a few days after a rain event and the wilting point " \
-        "those after a long drought.* ")
+        st.markdown( r""" *Table 1: Typical parameter values (after Chin, D.A.: Water-Resources Enginieering, 2013).*
+        Please note that for the use in this app, the values of $K_s$ need to be transformed from mm/h to mm/min.
+        The (field-)saturated water content $\theta_0$ can be approximated by value of porosity, 
+        the initial water content $\theta_i$ is likely to range between field capacity and wilting point, 
+        with the field capacitiy representing conditions a few days after a rain event and the wilting point 
+        those after a long drought.* """)
         st.markdown(""" 
                 | USD soil-texture class | Hydraulic conductivity [mm/h] | Suction head matrix potential [mm] | Porosity [-] | Field capacity [-] | Wilting point [-] |
                 |------------|:--------------------------------------:|:-----------------:|:-------------:|:-------------:|:----------:|
